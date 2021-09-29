f44306d7d1b5c544bc7e1cc12779bef46427ffe3
Федоров Александр <90206728+alex2002fedorov@users.noreply.github.com>
2021-09-29 23:37:08 +0300

    Федоров ПИ20-3
    2. Модифицируйте код клиента таким образом, чтобы он читал строки в цикле до тех пор, пока клиент не введет “exit”. Можно считать, что это команда разрыва соединения со стороны клиента.

--- client.py
+++ client.py
@@ -4,13 +4,11 @@ from time import sleep
 sock = socket.socket()
 sock.setblocking(1)
 sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
+#msg = "Hi!"
+msg = ''
+while (msg!='exit'):
+    msg = input()
+    sock.send(msg.encode())
+    data = sock.recv(1024)
 sock.close()
-
 print(data.decode())
