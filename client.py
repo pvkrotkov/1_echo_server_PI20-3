8fc2b5a58c729de6ced80c7434855755078828a3
kate kirillova <76176114+gyul-i@users.noreply.github.com>
2021-09-30 00:58:35 +0300

    добавила цикл (2 задание в лаб.)

--- client.py
+++ client.py
@@ -3,13 +3,17 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('localhost', 9090))
 
 #msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+msg = ""
+while True:
+    msg = input()
+    sock.send(msg.encode())
+    data = sock.recv(1024)
+    if msg == 'exit':
+        break
 
-data = sock.recv(1024)
 
 sock.close()
 
