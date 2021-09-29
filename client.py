fe5090eeb2c9e946bcad500bc9115a31855d712d
Федоров Александр <90206728+alex2002fedorov@users.noreply.github.com>
2021-09-29 22:24:59 +0300

    Update client.py
    Федоров Александр ПИ20-3

--- client.py
+++ client.py
@@ -5,12 +5,10 @@ sock = socket.socket()
 sock.setblocking(1)
 sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
+msg = ''
+while (msg!='exit'):
+    msg = input()
+    sock.send(msg.encode())
+    data = sock.recv(1024)
 sock.close()
-
 print(data.decode())
