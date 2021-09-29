5c68915999db9aa156b7f7ff59764774a324bc6e
Nian <76498608+AntonY4ZerO@users.noreply.github.com>
2021-09-29 23:55:02 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,22 @@
 import socket
 
+print('запуск сервера')
 sock = socket.socket()
 sock.bind(('', 9090))
+print('начало прослушивания порта')
 sock.listen(0)
 conn, addr = sock.accept()
+print('подключение пользователя')
 print(addr)
 
 msg = ''
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
+while msg!='exit':
+    data = conn.recv(1024)
+    msg = data.decode()
+    print('получение данных пользователя:')
+    print(msg)
+    conn.send(data)
 
 conn.close()
+print('остановка сервера')
