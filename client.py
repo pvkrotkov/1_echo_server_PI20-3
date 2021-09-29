6377553902a49cab2a7c3c72525bd0add6cc93bc
Nian <76498608+AntonY4ZerO@users.noreply.github.com>
2021-09-29 23:54:01 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,17 @@
 import socket
-from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
+sock.connect(('localhost', 9090))
+print('соединение с сервером')
+msg=''
+while (msg!='exit'):
+    print('ввод данных пользователя:')
+    msg = input()
+    print('отправка данных серверу')
+    sock.send(msg.encode())
+    data = sock.recv(1024)
 
 sock.close()
-
+print('отключение от сервера')
 print(data.decode())
