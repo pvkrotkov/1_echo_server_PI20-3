e7ce145a9fb0309f470b1d0e690a0b610bf77f5f
VVoroshilov <76512646+VVoroshilov@users.noreply.github.com>
2021-10-05 23:46:49 +0300

    Seminar 4-5 with some additional tasks
    Семинар 4-5
    Группа ПИ20-3
    Ворошилов Владислав
    Сделал основное задание и несколько дополнительных

--- server.py
+++ server.py
@@ -1,20 +1,40 @@
 import socket
+import datetime
 
+flag = False
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
+sock.bind(("", 1991))
+sock.listen(2)
+print(f"\33[93mServer launched on {datetime.datetime.now().strftime('%A, %d. %B %Y %I:%M%p')}\33[0m")
 
 while True:
-	data = conn.recv(1024)
-	if not data:
+	msg=''
+	conn, addr = sock.accept()
+	name = conn.recv(1024).decode().capitalize()
+	print(f'{name} \33[92mconnected!\33[0m')
+	while True:
+		data = conn.recv(1024)
+		msg = data.decode().capitalize()
+	
+		if msg == "Exit":
+			end_time = datetime.datetime.now().strftime('%A, %d. %B %Y %I:%M%p')
+			conn.send(f"\33[91mConnection has ended on {end_time}\33[90m".encode())
+			print(f"{name} \33[93mdisconnected\33[0m on {end_time}'\33[9m'")
+			conn.close()
+			break
+
+		elif msg == "Off":
+			print(f"\33[91mServer is turned off on {datetime.datetime.now().strftime('%A, %d. %B %Y %I:%M%p')} by user {name}\33[0m")
+			conn.close()
+			sock.close()
+			flag = True
+			break
+
+		else:
+			print(f"\33[7m{name.capitalize()}\33[0m: {msg.capitalize()}")
+			conn.send(f'\33[93mYour message sent on {datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")}\33[0m'.encode())
+
+	if flag:
 		break
-	msg += data.decode()
-	conn.send(data)
 
-print(msg)
-
-conn.close()
+sock.close()
