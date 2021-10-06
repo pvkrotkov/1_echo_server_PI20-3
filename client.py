37944df415c69c62789e3c0f70d74b0a45dfc6ba
Artem Vyrodov <dizpatcher@outlook.com>
2021-10-07 01:27:56 +0300

    stable logging

--- client.py
+++ client.py
@@ -1,14 +1,14 @@
 import socket
 import sys
-import threading
 import time
 
 BUFFERSIZE = 1024
 ADDR = '127.0.0.1'
 PORT = 9090
 SERVER = (ADDR, PORT)
-USER_PORT = 10090
+USER_PORT = 10092
 
+# функция проверки занятости порта
 def is_port_in_use(PORT):
 	try:
 		socket.socket(socket.AF_INET, socket.SOCK_DGRAM).bind((ADDR, PORT))
@@ -18,12 +18,12 @@ def is_port_in_use(PORT):
 	else:
 		return False
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # инициализация UDP-сокета
 request = input('Введите адрес и порт для подключения через пробел.  \n'\
                 'Нажмите Enter, чтобы использовать значение по умолчанию:\n')
 if request:
     try:
-        ADDR, USER_PORT = request.split()
+        ADDR, USER_PORT = request.split() # назначаем адрес и порт при пользовательском вводе
         PORT = int(USER_PORT)
     except:
         print('Введены некорректные данные!')
@@ -33,30 +33,32 @@ try:
     while is_port_in_use(USER_PORT):
         USER_PORT += 1
     print(f'Порт клиента - {USER_PORT}')
-    sock.bind((ADDR, USER_PORT))
+    sock.bind((ADDR, USER_PORT)) # присваиваем каждому клиенту уникальный порт
     print('Соединение с сервером...')
 except:
     print('Невозможно подключиться!')
     sys.exit()
 
 
-msg = f'.{ADDR}'
+msg = f'.{ADDR}' # отправляем стартовое сообщение на сервер - оно начинается с точки
 sock.sendto(msg.encode(), SERVER)
-while True:
-        data = sock.recv(BUFFERSIZE).decode()
-        print(data, '\n')
-        if data == 'Подключение установлено.':
-                break
-        msg = input('_  ')
-        sock.sendto(msg.encode(), SERVER)
+while True: # цикл авторизации на сервере
+	data, addr = sock.recvfrom(BUFFERSIZE)
+	data = data.decode()
+	print(data, '\n')
+	if data == 'Подключение установлено.': # при получении сообщения с таким текстом считаем пользователя залогиненным
+		break
+	msg = input('_ ')
+	sock.sendto(msg.encode(), SERVER)
 
+# основной цикл отправки сообщения на сервер
 while msg != 'exit':
 
     msg = input('текст сообщения>: ')
-    msg = '<пустое сообщение>' if not msg else msg
+    msg = '<пустое сообщение>' if not msg else msg # вместо '' отправляем текст <пустое сообщение>
     sock.sendto(msg.encode(), SERVER)
 
-    data = sock.recv(BUFFERSIZE)
+    data, addr = sock.recvfrom(BUFFERSIZE)
     print(data.decode() + '\n')
 
 sock.close()
--- server.py
+++ server.py
@@ -7,16 +7,16 @@ import time
 ADDR = '127.0.0.1'
 BUFFERSIZE = 1024
 PORT = 9090
-log_file = open('log.txt', 'w')
+log_file = open('log.txt', 'a') # при старте открываем файл логов
 
-def log(*args):
-	print(*args)
+def log(*args): # запись служебных сообщений в файл логов
+	print(args)
 	global log_file
 	log_file.write(f'[{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}] - ')
 	log_file.write(*args)
 	log_file.write('\n')
 
-def is_port_in_use(PORT):
+def is_port_in_use(PORT): # проверка занятости порта
 	try:
 		socket.socket(socket.AF_INET, socket.SOCK_DGRAM).bind((ADDR, PORT))
 		socket.socket().close()
@@ -25,12 +25,12 @@ def is_port_in_use(PORT):
 	else:
 		return False
 
-def find_user_by_id(addr):
+def find_user_by_id(addr): # поиск пользователя по id (порту)
 	with open('users.csv') as csvfile:
 		reader = csv.DictReader(csvfile, delimiter=';')
 		for row in reader:
 			if (int(row['id']) == int(addr[1])):
-				return row['name']
+				return row['name'] # возращаем имя, если находим запись о порте в базе
 		return False
 
 def is_password_correct(addr, pswd):
@@ -38,21 +38,21 @@ def is_password_correct(addr, pswd):
 		reader = csv.DictReader(csvfile, delimiter=';')
 		for row in reader:
 			if (int(row['id']) == int(addr[1])):
-				return pswd == row['pswd']
+				return pswd == row['pswd'] # проверяем корректность введенного пароля сравнивая хеши
 		return False
 
 
-def greet_client(sock, addr):
+def greet_client(sock, addr): # авторизация пользователя
 
-	if not find_user_by_id(addr):
+	if not find_user_by_id(addr): # если пользователь новый - просим представиться и ввести пароль
 		sock.sendto('Пожалуйста, представьтесь: '.encode(), addr)
 		name = sock.recvfrom(BUFFERSIZE)[0].decode()
 		sock.sendto('Введите пароль: '.encode(), addr)
-		pswd = hashlib.md5(sock.recv(BUFFERSIZE)).hexdigest()
-		with open('users.csv', 'a') as csvfile:
+		pswd = hashlib.md5(sock.recv(BUFFERSIZE)).hexdigest() # хешируем закодированный пароль md5
+		with open('users.csv', 'a') as csvfile: # записываем данные о новом пользователе в таблицу
 			writer = csv.DictWriter(csvfile, delimiter=';', fieldnames = ['id', 'ip', 'name', 'pswd'])
 			writer.writerow({'id': addr[1], 'ip': addr[0], 'name': name, 'pswd':pswd})
-	else:
+	else: # если пользователь уже зарегистрирован - приветствуем и сравниваем хеши паролей
 		sock.sendto(f'Добро пожаловать, {find_user_by_id(addr)}!\nВведите пароль для входа:\n'.encode(), addr)
 		pswd = hashlib.md5(sock.recv(BUFFERSIZE)).hexdigest()
 		while not is_password_correct(addr, pswd):
@@ -62,7 +62,7 @@ def greet_client(sock, addr):
 	sock.sendto('Подключение установлено.'.encode(), addr)
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # инициализация сокета
 log('Запуск сервера.')
 request = input('Введите адрес и порт сервера.  \n'\
                 'Нажмите Enter, чтобы использовать значение по умолчанию:\n')
@@ -75,7 +75,7 @@ if request:
         sys.exit()
 
 try:
-	while is_port_in_use(PORT):
+	while is_port_in_use(PORT): # при необходимости назначаем новый порт
 		print(f'Порт {PORT} занят.')
 		PORT += 1
 	sock.bind((ADDR, PORT))
@@ -89,10 +89,10 @@ except socket.error as e:
 msg = ''
 users = []
 while True:
-	data, addr = sock.recvfrom(BUFFERSIZE)
+	data, addr = sock.recvfrom(BUFFERSIZE) # получаем данные и адрес отправителя
 	log('Получение пакета')
 	data = data.decode()
-	if data[0] == '.':
+	if data[0] == '.': # если это первое сообщение (начинается с точки) - приветствуем и запрашиваем пароль / регистрируем
 		log('Подключение клиента...')
 		greet_client(sock, addr)
 		users.append(addr)
@@ -103,12 +103,13 @@ while True:
 		data, addr = sock.accept()
 
 	itsatime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-	print(f'[{addr[0]}]=[{str(addr[1])}]=[{itsatime}]| {data}')
+	print(f'[{addr[0]}]=[{str(addr[1])}]=[{itsatime}]| {data}') # выводим данные о сообщениях на сервер с указанием отправителя и времени
 
 	for user in users:
 		if user != addr:
-			sock.sendto(f'[{find_user_by_id(addr)}]: {data}'.encode(), user)
+			sock.sendto(f'[{find_user_by_id(addr)}]: {data}'.encode(), user) # пересылаем данные всем отличным от отправителя пользователям
 			log(f'Отправка данных на {user}.')
+	sock.sendto('\n'.encode(), addr)
 
 
 
