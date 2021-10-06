e1dfdf3c44fa2e9de5ac26ff449b3fe50be01721
Федоров Александр <90206728+alex2002fedorov@users.noreply.github.com>
2021-10-06 18:53:25 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,26 @@
 import socket
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+clients = []
 
-msg = ''
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('', 1443))
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+print("Server Started")
+cl = False
 
-print(msg)
+while not cl:
+    try:
+        data, addr = sock.recvfrom(1024)
 
-conn.close()
+        if addr not in clients:
+            clients.append(addr)
+            print(addr[1],'присоединился')
+
+        for client in clients:
+            if addr != client:
+                sock.sendto(data,client)
+    except:
+        print("\n[ Server Stopped ]")
+        cl = True
+
+sock.close()
