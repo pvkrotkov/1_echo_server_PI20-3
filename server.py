666347c9f33a3953f76338e8662ef79fb81d8c28
Sibilyov <90633901+Sibilyov@users.noreply.github.com>
2021-11-11 09:33:38 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 
 print('запуск сервера')
 sock = socket.socket()
-sock.bind(('', 9097))
+sock.bind(('', 9090))
 print('Начало прослушивания порта')
 sock.listen(0)
 conn, addr = sock.accept()
