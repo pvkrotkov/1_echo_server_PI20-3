d09b6f956f2a2ddd8920f58e27e1ba8986f0b93c
Sibilyov <90633901+Sibilyov@users.noreply.github.com>
2021-12-29 12:40:13 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,16 @@
+import socket
+
+sock = socket.socket()
+
+sock.bind(('',1777))
+
+sock.listen(1)
+
+conn, addr = sock.accept()    
+
+while True:   
+    data = conn.recv(1024)    
+    if not data:
+        break       
+    conn.send(data.upper())
+sock.close()
